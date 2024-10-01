3fef6e0fcf1afaf1d157ffa2d38611a9dd207e80
RodrigoAroeira <raroeirabr@gmail.com>
2024-10-01 12:26:52 -0300

    refactor: getPokemonInfo agora retorna um vetor ao invés de usar uma referência para manipular outro

--- utils.cpp
+++ utils.cpp
@@ -5,25 +5,27 @@
 #include <iostream>
 #include <vector>
 
-void getPokemonInfo(std::ifstream &arq, int maxRange,
-                    std::vector<Pokemon> &targetVec) {
+std::vector<Pokemon> getPokemonInfo(std::ifstream &arq, size_t size) {
 
-  for (int i = 0; i < maxRange; i++) {
+  std::vector<Pokemon> vec;
+  vec.reserve(size);
+
+  for (size_t i = 0; i < size; i++) {
     std::string nome, tipo;
     float ataque, defesa, vida;
 
     arq >> nome >> ataque >> defesa >> vida >> tipo;
 
-    Pokemon pokemon(nome, ataque, defesa, vida, tipo);
-    targetVec.push_back(pokemon);
+    vec.emplace_back(nome, ataque, defesa, vida, tipo);
   }
+  return vec;
 }
 
 std::array<Treinador, 2> lerTreinadores(const std::string nomeArq) {
   std::ifstream arquivo(nomeArq);
 
   if (!arquivo.is_open()) {
-    std::cerr << "Falha ao abrir o arqivo " << nomeArq << ", tente novamente."
+    std::cerr << "Falha ao abrir o arquivo " << nomeArq << ", tente novamente."
               << std::endl;
     exit(1);
   }
@@ -31,10 +33,9 @@ std::array<Treinador, 2> lerTreinadores(const std::string nomeArq) {
   int totalPokemons1, totalPokemons2;
 
   arquivo >> totalPokemons1 >> totalPokemons2;
-  std::vector<Pokemon> pokemons1, pokemons2;
 
-  getPokemonInfo(arquivo, totalPokemons1, pokemons1);
-  getPokemonInfo(arquivo, totalPokemons2, pokemons2);
+  std::vector<Pokemon> pokemons1 = getPokemonInfo(arquivo, totalPokemons1);
+  std::vector<Pokemon> pokemons2 = getPokemonInfo(arquivo, totalPokemons2);
 
   Treinador t1(pokemons1);
   Treinador t2(pokemons2);
