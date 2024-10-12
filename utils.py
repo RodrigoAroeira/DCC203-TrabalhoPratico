a4d9e3db21fa07bee5d41435b532f258ef73ad7c
RodrigoAroeira <raroeirabr@gmail.com>
2024-10-12 16:57:02 -0300

    refactor: loop para ler arquivo de treinadores movido para a própria função

--- utils.py
+++ utils.py
@@ -1,4 +1,23 @@
-from Pokemon import Pokemon, Treinador
+from typing import IO
+
+from Pokemon import Pokemon
+from Treinador import Treinador
+
+
+def readPokemons(arquivo: IO, size: int) -> list[Pokemon]:
+
+    pokemons = []
+
+    for _ in range(size):
+        lineParts = arquivo.readline().split()
+        nome = lineParts[0]
+        ataque = float(lineParts[1])
+        defesa = float(lineParts[2])
+        vida = float(lineParts[3])
+        tipo = lineParts[4]
+        pokemons.append(Pokemon(nome, ataque, defesa, vida, tipo))
+
+    return pokemons
 
 
 def lerTreinadores(nomeArq: str) -> tuple[Treinador, Treinador]:
@@ -6,24 +25,10 @@ def lerTreinadores(nomeArq: str) -> tuple[Treinador, Treinador]:
         qtd1, qtd2 = map(int, f.readline().strip().split())
 
         treinador1 = Treinador(qtd1)
-        for _ in range(qtd1):
-            lineParts = f.readline().split()
-            nome = lineParts[0]
-            ataque = float(lineParts[1])
-            defesa = float(lineParts[2])
-            vida = float(lineParts[3])
-            tipo = lineParts[4]
-            treinador1.pokemons.append(Pokemon(nome, ataque, defesa, vida, tipo))
+        treinador1.pokemons = readPokemons(f, qtd1)
 
         treinador2 = Treinador(qtd2)
-        for _ in range(qtd2):
-            lineParts = f.readline().split()
-            nome = lineParts[0]
-            ataque = float(lineParts[1])
-            defesa = float(lineParts[2])
-            vida = float(lineParts[3])
-            tipo = lineParts[4]
-            treinador2.pokemons.append(Pokemon(nome, ataque, defesa, vida, tipo))
+        treinador2.pokemons = readPokemons(f, qtd2)
 
         return treinador1, treinador2
 
