b82e09853844c32c08c0bb2e1caf3def6d262db1
RodrigoAroeira <raroeirabr@gmail.com>
2024-11-05 14:54:05 -0300

    tests: Novos testes para treinador

--- tests/test_treinador.cpp
+++ tests/test_treinador.cpp
@@ -1,4 +1,5 @@
 // #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
 #include "doctest.h"
 #include "treinador.hpp"
 #include "utils.hpp"
@@ -8,14 +9,20 @@ static std::array<Treinador, 2> treinadores = lerTreinadores("exemplo.txt");
 TEST_CASE("Treinador empty") {
   Treinador t({});
   CHECK(t.pokemons.empty());
-  CHECK_FALSE(t.isVencedor());
   CHECK(t.pokemonsVivos == 0);
   CHECK(t.pokemonsTotal == 0);
 }
 
 TEST_CASE("Treinador with pokemons") {
-  int pokemonsTotal =
+  size_t pokemonsTotal =
       treinadores[0].pokemonsTotal + treinadores[1].pokemonsTotal;
 
   CHECK(pokemonsTotal == 5);
+}
+
+TEST_CASE("Treinador winner") {
+  Treinador t({});
+  CHECK_FALSE(t.isVencedor());
+  t.makeVencedor();
+  CHECK(t.isVencedor());
 }
