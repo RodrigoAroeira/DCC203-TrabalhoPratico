1f91666d5f01484dd8f3adcb83441009c3d835d8
RodrigoAroeira <raroeirabr@gmail.com>
2024-11-20 22:40:23 -0300

    feat: adicionado gerador de short options e melhora de parsing

--- src/CLIParse.cpp
+++ src/CLIParse.cpp
@@ -6,29 +6,36 @@
 #include <locale>
 #include <string>
 
+#include "CLIParse.hpp"
 #include "pokemon.hpp"
 #include "treinador.hpp"
 #include "utils.hpp"
 
-struct option long_options[] = {{"help", no_argument, nullptr, 'h'},
-                                {"exemplo", no_argument, nullptr, 'e'},
-                                {"custom", required_argument, nullptr, 'c'},
-                                {"print", no_argument, nullptr, 'p'},
-                                {"instruct", no_argument, nullptr, 'i'},
-                                {nullptr, 0, nullptr, 0}};
+template <size_t size>
+std::string createShortOpts(const std::array<option, size> long_options) {
+  std::string shortOpts;
+  for (const option &opt : long_options) {
+    if (!opt.name)
+      continue;
+    shortOpts += opt.val;
+    shortOpts += std::string(opt.has_arg, ':');
+  }
 
-void help(const std::string &nomeArquivo) {
+  return shortOpts;
+}
+
+[[noreturn]] void help(const std::string &nomeArquivo) {
   std::cout << "Modo de uso: " << nomeArquivo << " [OPTION]\n"
             << "Options:\n"
-            << std::left << std::setw(25) << "  --help"
+            << std::left << std::setw(25) << "  --help, -h"
             << "Mostra essa mensagem e sai do programa.\n"
-            << std::left << std::setw(25) << "  --print"
+            << std::left << std::setw(25) << "  --print, -p"
             << "Imprime os treinadores e sai do programa.\n"
-            << std::left << std::setw(25) << "  --custom [arquivo]"
+            << std::left << std::setw(25) << "  --custom, -c [arquivo]"
             << "Lê de um arquivo .txt com um nome da sua escolha.\n"
-            << std::left << std::setw(25) << "  --exemplo"
+            << std::left << std::setw(25) << "  --exemplo, -e"
             << "Roda a batalha de exemplo. Funciona como --custom exemplo.txt\n"
-            << std::left << std::setw(25) << "  --instruct"
+            << std::left << std::setw(25) << "  --instruct, -i "
             << "Imprime as instruções de uso do programa e sai do programa.\n";
   exit(0);
 }
@@ -50,7 +57,7 @@ void printTreinadores(const std::array<Treinador, 2> &treinadores) {
   std::cout << "\033[0m";
 }
 
-void printInstruct(const std::string &nomeArquivo) {
+[[noreturn]] void printInstruct(const std::string &nomeArquivo) {
   std::locale::global(std::locale(""));
 
   std::ifstream arquivo(nomeArquivo);
@@ -64,8 +71,16 @@ void printInstruct(const std::string &nomeArquivo) {
     std::cout << buffer << '\n';
   }
 
-  arquivo.close();
+  exit(0);
 }
+
+auto long_options =
+    std::to_array<option>({{"help", no_argument, nullptr, 'h'},
+                           {"exemplo", no_argument, nullptr, 'e'},
+                           {"custom", required_argument, nullptr, 'c'},
+                           {"print", no_argument, nullptr, 'p'},
+                           {"instruct", no_argument, nullptr, 'i'},
+                           {nullptr, 0, nullptr, 0}});
 
 void parseArgs(int argc, char *argv[]) {
   int opt;
@@ -73,9 +88,10 @@ void parseArgs(int argc, char *argv[]) {
   std::string arquivo = "treinadores.txt";
 
   bool printFlag = false, batalhaFlag = true;
+  std::string shortOpts = createShortOpts(long_options);
 
-  while ((opt = getopt_long(argc, argv, "hec:p", long_options, &opt_index)) !=
-         -1) {
+  while ((opt = getopt_long(argc, argv, shortOpts.c_str(), long_options.data(),
+                            &opt_index)) != -1) {
     switch (opt) {
     case 'h':
       help(argv[0]);
@@ -92,8 +108,9 @@ void parseArgs(int argc, char *argv[]) {
       break;
     case 'i':
       printInstruct("instrucoes.txt");
-      return;
+      break;
     default:
+      help(argv[0]);
       break;
     }
   }
